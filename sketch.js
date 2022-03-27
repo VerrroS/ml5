0488e3886ed6b7af66d4351ba7be6864312c4a87
VerrroS <50030830+VerrroS@users.noreply.github.com>
2022-03-27 10:24:31 +0200

    add example images

--- sketch.js
+++ sketch.js
@@ -4,20 +4,13 @@ let classifier;
 // A variable to hold the image we want to classify
 let img;
 
-var exampleimages = ['images/clouds.png', 'images/portrait.png','images/sofa.png']
+var exampleimages = ['images/clouds.png', 'images/portrait.png', 'images/bird.png', 'images/laptop.png', 'images/salad.png', 'images/tree.png'];
+classifier = ml5.imageClassifier('MobileNet');
 
-function preload() {
-  classifier = ml5.imageClassifier('MobileNet');
-  img = loadImage('images/bird.png');
-}
+const image_active = document.getElementById('image_active');
+const inmage_input = document.getElementById('image_input');
 
-function setup() {
-  canvas = createCanvas(700, 400);
-  canvas.parent("canvas");
-  classifier.classify(img, gotResult);
-  image(img, 0, 0);
-}
-
+function setup(){}
 
 // A function to run when we get any errors and the results
 function gotResult(error, results) {
@@ -32,8 +25,8 @@ function gotResult(error, results) {
   }
   
   var trace1 = {
-    x: [results[0]["confidence"], results[1]["confidence"], results[2]["confidence"]],
-    y: [results[0]["label"], results[1]["label"], results[2]["label"]],
+    x: [results[2]["confidence"], results[1]["confidence"], results[0]["confidence"]],
+    y: [results[2]["label"], results[1]["label"], results[0]["label"]],
     orientation: 'h',
     marker: {
       color: 'rgba(55,128,191,0.6)',
@@ -43,8 +36,8 @@ function gotResult(error, results) {
   };
   
   var trace2 = {
-    x: [1-results[0]["confidence"], 1-results[1]["confidence"], 1-results[2]["confidence"]],
-    y: [results[0]["label"], results[1]["label"], results[2]["label"]],
+    x: [1-results[2]["confidence"], 1-results[1]["confidence"], 1-results[0]["confidence"]],
+    y: [results[2]["label"], results[1]["label"], results[0]["label"]],
     orientation: 'h',
     type: 'bar',
     marker: {
@@ -57,7 +50,7 @@ function gotResult(error, results) {
         barmode: 'stack',
         showlegend: false,
         height: 400,
-        width: 700,
+        width: 400,
       };
 
     var data = [trace1, trace2];
@@ -70,20 +63,53 @@ function createExamples(){
         newimage.style.height = '200';
         newimage.style.width = '200';
         newimage.setAttribute('data-id', i);
-        newimage.style.backgroundImage = 'url("' + exampleimages[i] + '")';
+        newimage.style.backgroundImage = `url(${exampleimages[i]})`;
         document.getElementById("exampleimages").appendChild(newimage);
-        newimage.addEventListener("click", changeImage);
+        newimage.addEventListener("click", function() { changeImage( exampleimages[this.dataset.id]); });
         //let prediction = classifier.predict(exampleimages[i], gotResults);
     }
 }
 
 
-function changeImage(){
-    img = loadImage(exampleimages[this.dataset.id]);
-    classifier.classify(img, gotResult);
-    let prediction = classifier.predict(img, gotResult);
-    console.log(prediction);
-    image(img, 0, 0);
+function changeImage(url){
+    img = loadImage(url, () => classifier.classify(img, gotResult));
+    image_active.style.backgroundImage = `url(${url})`;
+}
+
+function upload_image(){
+    const reader = new FileReader();
+    reader.addEventListener("load", () => {
+      const uploaded_image = reader.result;
+      changeImage(uploaded_image);
+ });
+    reader.readAsDataURL(this.files[0]);
 }
 
 createExamples();
+
+inmage_input.addEventListener("change", upload_image);
+image_active.addEventListener("dragover", function(ev) {ev.preventDefault()});
+image_active.addEventListener("drop", function(ev) {ev.preventDefault(), upload_image});
+
+ function dropHandler(ev) {
+    console.log('File(s) dropped');
+  
+    // Prevent default behavior (Prevent file from being opened)
+    ev.preventDefault();
+  
+    if (ev.dataTransfer.items) {
+      // Use DataTransferItemList interface to access the file(s)
+      for (var i = 0; i < ev.dataTransfer.items.length; i++) {
+        // If dropped items aren't files, reject them
+        if (ev.dataTransfer.items[i].kind === 'file') {
+          var file = ev.dataTransfer.items[i].getAsFile();
+          console.log('... file[' + i + '].name = ' + file.name);
+        }
+      }
+    } else {
+      // Use DataTransfer interface to access the file(s)
+      for (var i = 0; i < ev.dataTransfer.files.length; i++) {
+        upload_image(ev);
+      }
+    }
+  }
